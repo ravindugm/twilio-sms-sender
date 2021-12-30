6b91c7cd66785f7cdf716c53b9cdc550ec199573
ravindugm <ravindumiyuranga@gmail.com>
2021-12-31 00:34:24 +0530

    Initial commit

---
+++ src/test/java/com/rmdev/twiliosmssender/TwilioSmsSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmdev.twiliosmssender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioSmsSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
